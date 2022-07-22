e62ccdcaa8bd256f6cf2ab0ad383298e56dd8886
zhouqiang <2902511578@qq.com>
2022-07-22 17:28:02 +0800

    :sparkles: Introducing Java 8‘s feature

---
+++ intro-8/src/main/java/com/example/intro8/Intro8Application.java
@@ -0,0 +1,13 @@
+package com.example.intro8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class Intro8Application {
+
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(Intro8Application.class, args);
+	// }
+
+}
