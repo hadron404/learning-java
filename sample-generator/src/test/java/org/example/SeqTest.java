f3c94f40b3893e1c2edf8da66295d13eed4cc831
zhouqiang <2902511578@qq.com>
2023-04-14 17:53:52 +0800

    Define meta operator of Seq

--- sample-generator/src/test/java/org/example/SeqTest.java
+++ sample-generator/src/test/java/org/example/SeqTest.java
@@ -13,14 +13,13 @@ class SeqTest {
 	void test() {
 		List<Integer> list = Arrays.asList(1, 2, 3);
 		Seq<Integer> seq = list::forEach;
-		// 	等价于 Lambda Expression
-		// Seq<Integer> seq = consumer -> list.forEach(consumer);
-		// 	等价于 匿名内部类
-		// Seq<Integer> seq = new Seq<>() {
-		// 	@Override
-		// 	public void consume(Consumer<Integer> consumer) {
-		// 		list.forEach(consumer);
-		// 	}
-		// };
+		/*
+		* 等价于 Lambda Expression
+		* Seq<Integer> seq = consumer -> list.forEach(consumer);
+		* 亦等价于 匿名内部类
+		* Seq<Integer> seq = new Seq<>() { @Override public void consume(Consumer<Integer> consumer) { list.forEach(consumer); } };
+		*
+		* */
 	}
+
 }
